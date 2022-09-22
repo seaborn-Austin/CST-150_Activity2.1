a8f860088516fc35fad9c8e8b7b106161b78cde2
seaborn-Austin <64162552+seaborn-Austin@users.noreply.github.com>
2022-09-22 10:58:57 -0500

    Add project files.

---
+++ CST-150_Activity2.1/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CST_150_Activity2._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void wtBtn_Click(object sender, EventArgs e)
+        {
+            double wtPounds = double.Parse(wtLbTxt.Text);
+            double wtKg = wtPounds * 0.45359237;
+            wtKgTxt.Text = wtKg.ToString();
+        }
+    }
+}
